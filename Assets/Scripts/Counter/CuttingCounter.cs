f74aae429ab4d4cd0fd95ccc5f30aec5a76a1d6d
Zeky <nguyenphuoctoan2002@gmail.com>
2024-03-12 23:09:03 +0700

    CuttingRecipeSO

--- Assets/Scripts/Counter/CuttingCounter.cs
+++ Assets/Scripts/Counter/CuttingCounter.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CuttingCounter : BaseCounter
 {
-    [SerializeField] private KitchenObjectSO kitchenObjectSO;
+    [SerializeField] private List<CuttingRecipeSO> cuttingRecipeSOArray;
 
     public override void Interact(Player player)
     {
@@ -40,11 +40,39 @@ public class CuttingCounter : BaseCounter
 
     public override void InteractAlternative(Player player)
     {
-        if (HasKitchenObject())
+        KitchenObjectSO kitchenObjectSO = GetKitchenObject().GetKitchenScriptableSO();
+
+        if (HasKitchenObject() && HasRecipeForInput(kitchenObjectSO))
         {
             GetKitchenObject().DestroySelf();
 
-            KitchenObject.Spawn(kitchenObjectSO, this);
+            KitchenObject.Spawn(GetOuputFromInput(kitchenObjectSO), this);
         }
+    }
+
+    private bool HasRecipeForInput(KitchenObjectSO kitchenObjectSO)
+    {
+        foreach (CuttingRecipeSO item in cuttingRecipeSOArray)
+        {
+            if (kitchenObjectSO == item.input)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private KitchenObjectSO GetOuputFromInput(KitchenObjectSO kitchenObjectSO)
+    {
+        foreach (CuttingRecipeSO item in cuttingRecipeSOArray)
+        {
+            if (kitchenObjectSO == item.input)
+            {
+                return item.output;
+            }
+        }
+
+        return null;
     }
 }
