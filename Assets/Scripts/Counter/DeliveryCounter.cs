b88bc8e1091e50a02f6fb0214b8f5983e0640d34
Zeky <nguyenphuoctoan2002@gmail.com>
2024-03-15 15:00:43 +0700

    DeliveryArrow Shader

---
+++ Assets/Scripts/Counter/DeliveryCounter.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeliveryCounter : BaseCounter
+{
+    public override void Interact(Player player)
+    {
+        //Player has a kitchenObject
+        if (player.HasKitchenObject())
+        {
+            //Player is holding a plate
+            if (player.GetKitchenObject().TryGetPlateKitchenObject(out var kitchenObject))
+            {
+                player.GetKitchenObject().DestroySelf();
+            }
+        }
+    }
+}
